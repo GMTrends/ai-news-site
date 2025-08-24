53054a168fb63042a6a70f3c93449bfde7d125cc
unknown <Raf>
2025-08-24 01:48:08 -0400

    Auto-commit before backup

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly EMAILOCTOPUS_API_KEY: string;
+  readonly EMAILOCTOPUS_LIST_ID: string;
+  readonly SITE: string;
+  readonly NODE_ENV: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
